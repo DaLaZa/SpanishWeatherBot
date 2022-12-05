af1a4b6c27a5ea9db8598781292d025db3e532d7
DaLaZa <DaLaZa@github.com>
2022-12-05 19:38:54 +0100

    Create log dir if not exists

--- AemetBot/utils/logger.py
+++ AemetBot/utils/logger.py
@@ -1,11 +1,18 @@
 from AemetBot import _ROOT_DIR
 from datetime import datetime
 import logging
+import os
 
 # Create log application
 date_time = datetime.today()
 date_today_format = "%Y-%m-%d %H:%M:%S.%f"
 date_today = str(datetime.strptime(str(date_time), date_today_format).strftime("%Y%m%d"))
+
+# Chek if log dir exists if not create it
+isExist = os.path.exists(os.path.join(_ROOT_DIR, "log"))
+if not isExist:
+    os.makedirs(os.path.join(_ROOT_DIR, "log"))
+
 log_file = f'{_ROOT_DIR}/log/application_log_{date_today}'
 logging.basicConfig(filename=log_file, level=logging.ERROR, format="%(asctime)s; %(message)s", filemode="a+")
 
